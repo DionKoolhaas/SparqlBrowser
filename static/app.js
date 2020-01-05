2569a635bd1c119241f15ea2b7fd3a988506b987
Dion <dion12@hotmail.com>
2020-01-05 17:15:26 +0100

    nodes zijn weer sleppbaar

--- static/app.js
+++ static/app.js
@@ -13,9 +13,11 @@ var svg = d3.select("body").append("svg")
 
 var simulation = d3.forceSimulation(nodes)
   .force('center', d3.forceCenter(width / 2, height / 2))
-  .force('charge', d3.forceManyBody().strength(-100))
+  //.force('charge', d3.forceManyBody().strength(4000))
+  .force("forceX", d3.forceX().strength(.1).x(width * .5))
+          .force("forceY", d3.forceY().strength(.1).y(height * .5))
   .force('collision', d3.forceCollide().radius(function(d) {
-      return 20
+      return 50
     }))
   .on('tick', ticked);
 
@@ -29,16 +31,34 @@ console.log("you are being ticked")
     .append('g')
     .attr('class', 'node')
     .append('circle')
-    .attr('r', 5)
+    .attr('r', 50)
     .style('fill', function(d) { return color(d.bron)})
-    .merge(u)
-    .select('circle')
-    .attr('cx', function(d) {
-      return d.x
-    })
-    .attr('cy', function(d) {
-      return d.y
-    })
+    .call(d3.drag()
+                  .on("start", dragstarted)
+                  .on("drag", dragged)
+                  .on("end", dragended));
+    //.merge(u)
+    //.select('circle')
+
+    u.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
 
   u.exit().remove()
 }
+
+function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(.03).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(.03);
+      d.fx = null;
+      d.fy = null;
+    }
