ca53cac29264c7e202a3dc0cdf38a6b50e72517a
Dion <dion12@hotmail.com>
2020-01-08 22:18:52 +0100

    inzoomen en slepen is nu mogelijk. en tekst van de node is nu rechts boven de node geplaatst

--- static/app.js
+++ static/app.js
@@ -1,6 +1,7 @@
 
 var width = 2000,
-    height = 1000;
+    height = 1000,
+    circleRadius = 10;
 
 //stop een object in color, en je krijgt een html kleur terug
 var color = d3.scaleOrdinal(d3.schemeSet1);
@@ -9,20 +10,32 @@ var color = d3.scaleOrdinal(d3.schemeSet1);
 //voeg een svg toe aan de dom
 var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    //inzoomen mogelijk maken
+    .call(
+        d3.zoom()
+            .scaleExtent([.1, 4])
+            .on("zoom", function() { container.attr("transform", d3.event.transform); })
+    );
+
+var container = svg.append("g");
 
 var simulation = d3.forceSimulation(graph.nodes)
   .force('center', d3.forceCenter(width / 2, height / 2))
-  .force('charge', d3.forceManyBody().strength(-6000))
+  .force('charge', d3.forceManyBody().strength(-3000))
   .force('link', d3.forceLink().links(graph.links).id(function(d) { return d.uri; }).distance(100))
+  .force("x", d3.forceX(width / 2).strength(1))
+  .force("y", d3.forceY(height / 2).strength(1))
   .on('tick', ticked);
 
-var links = svg.selectAll('line')
+var links = container.selectAll('.link')
     .data(graph.links)
     .enter()
+    .append('g')
+    .attr('class', '.link')
     .append('line');
 
-var nodes = svg
+var nodes = container
     .selectAll('.node')
     .data(graph.nodes).enter()
     .append('g')
@@ -32,13 +45,16 @@ var nodes = svg
                   .on("drag", dragged)
                   .on("end", dragended));
   nodes.append('circle')
-    .attr('r', 50)
+    .attr('r', circleRadius)
     .style('fill', function(d) { return color(d.bron)})
     ;
 
   nodes.append("text")
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle")
+      .style("font-family", "Roboto")
+      .attr("text-anchor", "left")
+      //tekst rechtsboven van circel plaatsen
+      .attr("x", circleRadius)
+      .attr("y", -circleRadius)
       .text(function(d) { return d.uri.substr(d.uri.lastIndexOf('/') + 1); });
 
 function updateNodes() {
